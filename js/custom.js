4ac0da1b29245edfabdf3ba0517fca3ede6db52b
Olexii Bulhakov <bulhakovolexii@gmail.com>
2024-12-23 22:14:29 +0200

    fix: add some design of changes by Valentin

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+function toggleMenu() {
+    const menu = document.getElementById('primary-menu')
+    const container = document.getElementById('main-nav')
+    const viewport = window.innerWidth
+    const navbar = document.querySelector('#main-nav')
+
+    let menuWidth = 0
+    const menuItems = menu.querySelectorAll(':scope > li')
+    menuItems.forEach((item) => {
+        menuWidth += item.offsetWidth
+    })
+
+    const containerWidth = container.offsetWidth
+
+    if (viewport > 1300 && menuWidth > containerWidth - 250) {
+        navbar.classList.add('menu-hidden')
+    } else if (viewport > 1000 && menuWidth > containerWidth - 200) {
+        navbar.classList.add('menu-hidden')
+    } else {
+        navbar.classList.remove('menu-hidden')
+    }
+}
+
+window.addEventListener('load', toggleMenu)
+window.addEventListener('resize', toggleMenu)
