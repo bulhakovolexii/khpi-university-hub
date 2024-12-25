bccb17de9662a108aa9ebe6f224a97f83105021f
Olexii Bulhakov <bulhakovolexii@gmail.com>
2024-12-25 20:48:57 +0200

    fix: fixed comments in footer.php

--- footer.php
+++ footer.php
@@ -9,44 +9,13 @@
  * @package University_Hub
  */
 
-/**
- * Hook - university_hub_action_after_content.
- *
- * @hooked university_hub_content_end - 10
- */
 do_action('university_hub_action_after_content'); ?>
 
-	 /**
- * Hook - university_hub_action_before_footer.
- *
- * @hooked university_hub_add_footer_bottom_widget_area - 5
- * @hooked university_hub_footer_start - 10
- */<?php
+	<?php do_action('university_hub_action_before_footer'); ?>
+    <?php do_action('university_hub_action_footer'); ?>
+	<?php do_action('university_hub_action_after_footer'); ?>
 
-do_action('university_hub_action_before_footer'); ?>
-     /**
- * Hook - university_hub_action_footer.
- *
- * @hooked university_hub_footer_copyright - 10
- */<?php
-
-do_action('university_hub_action_footer'); ?>
-	 /**
- * Hook - university_hub_action_after_footer.
- *
- * @hooked university_hub_footer_end - 10
- */<?php
-
-do_action('university_hub_action_after_footer'); ?>
-
- /**
- * Hook - university_hub_action_after.
- *
- * @hooked university_hub_page_end - 10
- * @hooked university_hub_footer_goto_top - 20
- */<?php
-
-do_action('university_hub_action_after'); ?>
+<?php do_action('university_hub_action_after'); ?>
 
 <?php wp_footer(); ?>
 </body>
