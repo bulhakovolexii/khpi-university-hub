8a057022685ba8ec0f3acb340908c43b5e566656
Olexii Bulhakov <bulhakovolexii@gmail.com>
2024-12-25 16:11:27 +0200

    feat: added adaptive hiding of the search field

--- js/custom.js
+++ js/custom.js
@@ -1,25 +1,40 @@
 function toggleMenu() {
-    const menu = document.getElementById('primary-menu')
     const container = document.getElementById('main-nav')
+    const menu = document.getElementById('primary-menu')
     const viewport = window.innerWidth
-    const navbar = document.querySelector('#main-nav')
-
-    let menuWidth = 0
+    const containerWidth = container.offsetWidth
     const menuItems = menu.querySelectorAll(':scope > li')
+    let menuWidth = 0
     menuItems.forEach((item) => {
         menuWidth += item.offsetWidth
     })
-
-    const containerWidth = container.offsetWidth
-
     if (viewport > 1300 && menuWidth > containerWidth - 256) {
-        navbar.classList.add('menu-hidden')
+        container.classList.add('menu-hidden')
     } else if (viewport > 1000 && menuWidth > containerWidth - 206) {
-        navbar.classList.add('menu-hidden')
+        container.classList.add('menu-hidden')
     } else {
-        navbar.classList.remove('menu-hidden')
+        container.classList.remove('menu-hidden')
     }
 }
 
-window.addEventListener('load', toggleMenu)
+function addToggleButton() {
+    const container = document.getElementById('main-nav')
+    const button = document.createElement('button')
+    button.id = 'show-search'
+    button.textContent = ''
+    container.appendChild(button)
+    button.addEventListener('click', toggleMenuManually)
+}
+
+function toggleMenuManually() {
+    const container = document.getElementById('main-nav')
+    if (container.classList.contains('menu-hidden')) {
+        container.classList.remove('menu-hidden')
+    }
+}
+
+window.addEventListener('load', () => {
+    toggleMenu()
+    addToggleButton()
+})
 window.addEventListener('resize', toggleMenu)
