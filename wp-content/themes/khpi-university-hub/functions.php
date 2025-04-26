82eecdf94b19a6be9365f403364c73ebab2ab419
Olexii Bulhakov <bulhakovolexii@gmai.com>
2025-04-26 15:38:20 +0300

    feat: Added Open Graph Tags

--- wp-content/themes/khpi-university-hub/functions.php
+++ wp-content/themes/khpi-university-hub/functions.php
@@ -475,3 +475,49 @@ add_filter('wp_handle_upload', function ($file) {
 
     return $file;
 });
+
+/**
+ * Adding Open Graph Tags
+ */
+function doctype_opengraph($output)
+{
+    return $output .
+        '
+    xmlns:og="http://opengraphprotocol.org/schema/"
+    xmlns:fb="http://www.facebook.com/2008/fbml"';
+}
+add_filter('language_attributes', 'doctype_opengraph');
+
+function fb_opengraph()
+{
+    global $post;
+
+    if (is_single()) {
+
+        if (has_post_thumbnail($post->ID)) {
+            $img_data = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large');
+            $img_src = $img_data ? $img_data[0] : get_stylesheet_directory_uri() . '/assets/images/opengraph_image.png';
+        } else {
+            $img_src = get_stylesheet_directory_uri() . '/assets/images/opengraph_image.png';
+        }
+        if ($excerpt = $post->post_excerpt) {
+            $excerpt = strip_tags($post->post_excerpt);
+            $excerpt = str_replace('', "'", $excerpt);
+        } else {
+            $excerpt = get_bloginfo('description');
+        }
+        ?>
+ 
+    <meta property="og:title" content="<?php echo the_title(); ?>"/>
+    <meta property="og:description" content="<?php echo $excerpt; ?>"/>
+    <meta property="og:type" content="article"/>
+    <meta property="og:url" content="<?php echo the_permalink(); ?>"/>
+    <meta property="og:site_name" content="<?php echo get_bloginfo(); ?>"/>
+    <meta property="og:image" content="<?php echo $img_src; ?>"/>
+ 
+<?php
+    } else {
+        return;
+    }
+}
+add_action('wp_head', 'fb_opengraph', 5);
